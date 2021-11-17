5e84e1393e98246e08d5fbff8bf620ab7469b3c3
Arseniy Kononov <kononovarseniy@gmail.com>
2021-11-17 10:04:13 +0700

    Add MainPLL

--- UART.vhd
+++ UART.vhd
@@ -6,12 +6,25 @@ use ieee.numeric_std.all;
 entity UART is
 	port (
 		clk_i: in std_logic
-		
-		
 	);
 end entity;
 
 architecture rtl of UART is
+
+-- MainPLL
+signal clk_s: std_logic;
+component MainPLL is
+	port (
+		inclk0: in std_logic := '0';
+		c0: out std_logic
+	);
+end component;
+
 begin
+	main_pll: MainPLL
+		port map (
+			inclk0 => clk_i,
+			c0 => clk_s
+		);
 
 end architecture;
