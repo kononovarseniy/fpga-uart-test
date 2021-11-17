bac1955dd51dd8b487aad85a8d6cbcc44c7eae40
Arseniy Kononov <kononovarseniy@gmail.com>
2021-11-17 09:47:03 +0700

    Initial commit

---
+++ UART.vhd
@@ -0,0 +1,17 @@
+library ieee;
+
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity UART is
+	port (
+		clk_i: in std_logic
+		
+		
+	);
+end entity;
+
+architecture rtl of UART is
+begin
+
+end architecture;
